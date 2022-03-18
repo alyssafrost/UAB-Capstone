5a20e65ea5b7cc5eadeead815660404e35c53ab6
Alyssa Frost <frost7@uab.edu>
2022-03-18 14:25:47 -0500

    Stub db keys

--- db_schema.sql
+++ db_schema.sql
@@ -3,3 +3,21 @@ CREATE TYPE boolean_attr
 CREATE TYPE integer_attr 
 
 -- Stubbed creation types for SQL schema (come back to this)
+-- We need these things:
+-- (
+-- 'window', 
+-- 'door',
+-- 'light',
+-- 'oven',
+-- 'stove',
+-- 'refridgerator',
+-- 'microwave',
+-- 'dishwasher',
+-- 'shower',
+-- 'bathtub',
+-- 'bathroomfan',
+-- 'mainroomtelevision',
+-- 'bedroomtelevision',
+-- 'washingmachine',
+-- 'dryer'
+-- );
