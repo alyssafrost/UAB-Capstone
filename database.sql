d24ce6cd912d0cc7bbcc3340ca0eb6afe2a42ada
Alyssa Frost <frost7@uab.edu>
2022-03-29 18:37:56 -0500

    Update the simple database

---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS calculations (
+    DAY AS DAY integer NOT NULL,
+    water_cost decimal NOT NULL,
+    electricity_cost decimal NOT NULL,
+    total_cost decimal NOT NULL,
+)
