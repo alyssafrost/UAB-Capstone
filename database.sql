43b6fa74d496c20dffa7aa2cb4acc2bf47f6edaa
Alyssa Frost <frost7@uab.edu>
2022-04-05 21:56:33 +0000

    Create DATE as primary key

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS calculations (
-    DAY AS DAY integer NOT NULL,
+    record_date DATE PRIMARY KEY,
     water_cost decimal NOT NULL,
     electricity_cost decimal NOT NULL,
     total_cost decimal NOT NULL,
