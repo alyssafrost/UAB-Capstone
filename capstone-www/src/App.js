b96740fe50ee07d7e5db85f661250a4af798c204
Alyssa Frost <frost7@uab.edu>
2022-04-06 22:58:39 -0500

    Clean deleted files

--- capstone-www/src/App.js
+++ capstone-www/src/App.js
@@ -2,26 +2,14 @@ import logo from "./logo.svg";
 import "./App.css"; // Import all of the Routers, switches, routes, redirects here.
 import Button from "react-bootstrap/Button";
 
-import HomeScreen from "./components/HomeScreen";
-import AnalysisScreen from "./components/AnalysisScreen";
-import ControlScreen from "./components/ControlScreen";
-
-import { Data } from "./components/DataTable";
+import { MenuBar } from "./components/MenuBar";
+import { Data } from "./components/AnalysisScreen";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>test render</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MenuBar />
       </header>
     </div>
   );
--- capstone-www/src/components/DataTable.js
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// import { Table } from "react-bootstrap";
-
-// Data = { // this is usually a type script cast
-//   waterUse: number,
-//   electricalUse: number,
-//   totalCost: number,
-// };
