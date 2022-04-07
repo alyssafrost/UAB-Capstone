7503524cbad7142dbb77d9bf8f777e8b2e087b57
Alyssa Frost <frost7@uab.edu>
2022-04-06 22:59:30 -0500

    Fix trailing comma

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS calculations (
-    record_date DATE PRIMARY KEY,
+    record_date DATE PRIMARY KEY, 
     water_cost decimal NOT NULL,
     electricity_cost decimal NOT NULL,
-    total_cost decimal NOT NULL,
+    total_cost decimal NOT NULL
 )
