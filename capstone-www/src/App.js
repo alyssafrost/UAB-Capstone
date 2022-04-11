e349d007c133191d4f77c62133e82b12eb7fbe1e
Alyssa Frost <frost7@uab.edu>
2022-04-11 14:41:40 -0500

    Bump comment

--- capstone-www/src/App.js
+++ capstone-www/src/App.js
@@ -1,9 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css"; // Import all of the Routers, switches, routes, redirects here.
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 
 import { MenuBar } from "./components/MenuBar";
-import { Data } from "./components/AnalysisScreen";
+// import { Data } from "./components/AnalysisScreen";
 
 function App() {
   return (
