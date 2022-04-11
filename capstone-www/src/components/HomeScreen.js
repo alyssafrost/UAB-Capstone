2e861f60df6340b98dd5b366dec361c4099abd5b
Alyssa Frost <frost7@uab.edu>
2022-04-11 14:42:11 -0500

    Update index

--- capstone-www/src/components/HomeScreen.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Floorplan from "./Floorplan"; // include the floorplan
-import Thermostat from "./Thermostat"; // include the thermostat
-
-const HomeScreen = () => {
-  return <img src={require("../imgs/FloorPlan.jpg")} alt=""></img>;
-};
-export { HomeScreen };
