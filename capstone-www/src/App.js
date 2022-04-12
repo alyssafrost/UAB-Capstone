b39cc58d840185da848f2fd2f76edb0bb4aef003
Alyssa Frost <frost7@uab.edu>
2022-04-11 20:09:26 -0500

    Add imports

--- capstone-www/src/App.js
+++ capstone-www/src/App.js
@@ -1,4 +1,6 @@
+import { Divider } from "antd";
 import "./App.css"; // Import all of the Routers, switches, routes, redirects here.
+import { HomeScreen, TemperatureBox } from "./components/HomeScreen";
 // import Button from "react-bootstrap/Button";
 
 import { MenuBar } from "./components/MenuBar";
