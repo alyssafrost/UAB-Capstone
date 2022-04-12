45dc938d3740b6b0d342db54d7e886ad52a06956
Alyssa Frost <frost7@uab.edu>
2022-04-11 20:10:30 -0500

    Add switches; TODOs

--- capstone-www/src/components/ControlScreen.js
+++ capstone-www/src/components/ControlScreen.js
@@ -1,30 +1,40 @@
 import { Layout } from "antd";
 import { Switch } from "antd";
 
-// function onChange(checked) {
-//   console.log(`switch to ${checked}`);
-// }
-
-// ReactDOM.render(<Switch defaultChecked onChange={onChange} />, mountNode);
-
-// const Switch = (checked) => {
-//   console.log();
-// };
-
 const { Header, Footer, Sider, Content } = Layout;
 
+function onChange(checked) {
+  console.log(`switch to ${checked}`);
+}
+
+const ApplianceToggle = () => {
+  return (
+    <Switch defaultChecked onChange={onChange}>
+      <br />
+      <Switch size="default" defaultChecked />
+      document.getElementById('container')
+    </Switch>
+  );
+};
+
+// fix tiny later // use Space to define the sizes between switches, or Grid, or Divider
 const ControlScreen = () => {
   return (
     <Layout>
-      {"Layout Text"}
+      {"Switch Toggles"}
       "Test Render"
       <Header>
         {"Header"}
         "Test Header"
-        <Content>"Test Content"</Content>
+        <Content>
+          {" "}
+          <ApplianceToggle>"1"</ApplianceToggle>
+          <ApplianceToggle>"2"</ApplianceToggle>
+        </Content>
       </Header>
     </Layout>
   );
 };
 
 export { ControlScreen };
+export { ApplianceToggle };
