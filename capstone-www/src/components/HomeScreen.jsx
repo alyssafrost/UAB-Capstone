e0dcac2afe2014b5b97414bb53f0c808a2f347c1
Alyssa Frost <frost7@uab.edu>
2022-04-11 20:09:59 -0500

    Test functional Grid structure over Divide

--- capstone-www/src/components/HomeScreen.jsx
+++ capstone-www/src/components/HomeScreen.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 import { Card } from "antd";
+import { Grid } from "antd";
+import { Row, Col } from "antd";
 
 // import Floorplan from "./Floorplan"; // include the floorplan
 // import Thermostat from "./Thermostat"; // include the thermostat
+
+// Probably remove Grid, contextualize to just rely on divisor placement.
+const gridLayout = () => {
+  return (
+    <Row>
+      <Col span={24}> </Col>
+      <Col span={12}>col-12</Col>
+      <Col span={8}>col-8</Col>
+    </Row>
+  );
+};
 
 const HomeScreen = () => {
   return <img src={require("../imgs/FloorPlan.jpg")} alt=""></img>;
 };
 
 const TemperatureBox = () => {
-  return <Card title="Temperature Control">test</Card>;
+  return <Card title="Temperature Control"></Card>;
 };
 
-// // not sure if this is an indexable thing to div page?
-// {
-//   array.map((TemperatureBox) => <li>{TemperatureBox.id}</li>);
-// }
+const intExtTemp = () => {
+  return <Card title="Real Time temperature">test</Card>;
+};
 
-export { HomeScreen, TemperatureBox };
+const generalLighting = () => {
+  return <Card title="Light Control">test</Card>;
+};
+
+export {
+  gridLayout,
+  HomeScreen,
+  TemperatureBox,
+  // nextScreenToggle,
+  intExtTemp,
+  generalLighting,
+};
