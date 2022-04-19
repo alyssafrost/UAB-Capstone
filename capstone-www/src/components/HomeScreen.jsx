628706c48244a1f90dbc715e250d4ca18afbf3c3
Alyssa Frost <frost7@uab.edu>
2022-04-18 22:05:15 -0500

    Improve button-icon functioanlity

--- capstone-www/src/components/HomeScreen.jsx
+++ capstone-www/src/components/HomeScreen.jsx
@@ -1,29 +1,36 @@
+// React, antd
 import React from "react";
 import { Card } from "antd";
 import { Space } from "antd";
 import { UpOutlined, DownOutlined } from "@ant-design/icons";
+import { Switch } from "antd";
+import { Button } from "antd";
 
+import "../antd.css";
+
+// Local exports
 export const HomeScreen = () => {
   return <img src={require("../imgs/FloorPlan.jpg")} alt=""></img>;
 };
-
-// const tabList = [ {
-//   key: 'tab1',
-//   tab: 'tab1',
-// },
-
-// {
-//   key: 'tab2',
-//   tab: 'tab2',
-// }
 
 //put these in ghost buttons
 export const TemperatureBox = () => {
   return (
     <Card title="Temperature Control">
-      <UpOutlined style={{ fontSize: "44px" }} />
-      <DownOutlined style={{ fontSize: "44px" }} />
-      <p style={{ fontSize: "34px" }}>76°</p>
+      <span className="site-button-ghost-wrapper-up" align="left">
+        <Button type="primary" ghost size="large">
+          <UpOutlined style={{ fontSize: "34px" }} align="left" />
+        </Button>
+      </span>
+      <Space>
+        <Space></Space>
+        <span className="site-button-ghost-wrapper-down" align="right">
+          <Button type="primary" ghost size="large">
+            <DownOutlined style={{ fontSize: "34px" }} align="right" />
+          </Button>
+        </span>
+      </Space>
+      <p style={{ fontSize: "34px" }}> 76° </p>
     </Card>
   );
 };
@@ -33,11 +40,11 @@ export const IntExtTemp = () => {
     <Card title="Real-time Temperature">
       {/* <h align="top"> YES </h> */}
       <Space direction="vertical">
-        <p style={{ fontSize: "20px" }} align="center">
-          Internal: <p style={{ fontSize: "25px" }}> 76° </p>
+        <p style={{ fontSize: "23px" }} align="center">
+          Internal: <p style={{ fontSize: "27px" }}> 76° </p>
         </p>
-        <p style={{ fontSize: "20px" }} align="center">
-          External: <p style={{ fontSize: "25px" }}> 85° </p>
+        <p style={{ fontSize: "23px" }} align="center">
+          External: <p style={{ fontSize: "27px" }}> 85° </p>
         </p>
       </Space>
     </Card>
@@ -47,7 +54,11 @@ export const IntExtTemp = () => {
 export const GeneralLighting = () => {
   return (
     <Card title="Light Control">
-      <p>Test</p>
+      <Space direction="vertical" align="left">
+        <Switch></Switch>
+        <Switch></Switch>
+        <Switch></Switch>
+      </Space>
     </Card>
   );
 };
