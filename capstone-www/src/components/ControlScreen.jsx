3aa8b049e879ee1506c5b647be244233f53f7128
Alyssa Frost <frost7@uab.edu>
2022-04-18 23:13:36 -0500

    Add subtext to switches

--- capstone-www/src/components/ControlScreen.jsx
+++ capstone-www/src/components/ControlScreen.jsx
@@ -31,61 +31,75 @@ const ControlScreen = () => {
       {"Switch Toggles"}
       <Card title="Kitchen Appliances" align="horizontal">
         <Space>
+          <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Dish Washer </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Stove </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Oven </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Refridgerator </h>
           <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-        </Space>
-      </Card>
-      <Card title="Main Bedroom Appliances" align="horizontal">
-        <Space>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-        </Space>
-      </Card>
-      <Card title="Main Bathroom" align="horizontal">
-        <Space>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
+          <h> Microwave </h>
           <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
       <Card title="Living Room Appliances" align="horizontal">
         <Space>
+          <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Lamp 1 </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Lamp 2 </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Television </h>
           <ApplianceToggle></ApplianceToggle>
+        </Space>
+      </Card>
+      <Card title="Main Bedroom Appliances" align="horizontal">
+        <Space>
+          <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Lamp 1 </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Lamp 2 </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Television </h>
           <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
       <Card title="Kids Bedroom Appliances" align="horizontal">
         <Space>
+          <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Lamp 1 </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Lamp 2 </h>
           <ApplianceToggle></ApplianceToggle>
+        </Space>
+      </Card>
+      <Card title="Main Bathroom" align="horizontal">
+        <Space>
+          <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
+          <h> Exhaust Fan </h>
           <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
       <Card title="Kids Bathroom" align="horizontal">
         <Space>
+          <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Exhaust Fan </h>
           <ApplianceToggle></ApplianceToggle>
+        </Space>
+      </Card>
+      <Card title="Wash Room" align="horizontal">
+        <Space>
+          <h> Washing Machine </h>
           <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
-          <ApplianceToggle></ApplianceToggle>
+          <h> Dryer </h>
           <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
