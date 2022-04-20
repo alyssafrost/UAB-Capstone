78d59fb1f57fb7cb20f27e41883dab18be71ee10
Alyssa Frost <frost7@uab.edu>
2022-04-19 21:10:47 -0500

    Update index.js

--- capstone-www/src/index.js
+++ capstone-www/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./MenuBar.css";
+import "./Popup.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
