a2e921dee45670861657b0d3d70e116a59b1d6d3
Alyssa Frost <frost7@uab.edu>
2022-04-19 21:07:56 -0500

    Add duration timer

--- capstone-www/src/App.jsx
+++ capstone-www/src/App.jsx
@@ -1,13 +1,33 @@
-import "./App.css"; // Import all of the Routers, switches, routes, redirects here.
-
+import { useState, useEffect } from "react";
 import { MenuBar } from "./components/MenuBar";
 
+// Local
+import { Popup } from "./components/Popup";
+
+// css
+import "./App.css";
+import "./Popup.css";
+
 function App() {
+  const [timedPopup, setTimedPopup] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimedPopup(true);
+    }, 30000); // pops up 30 seconds into app
+  }, []);
   return (
     <div className="App">
+      <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
+        <h3> WARNING</h3>
+        <p>
+          {" "}
+          Using this app in excess can be mentally draining. It is recommended
+          to stand up and rehydrate.
+        </p>
+      </Popup>
       <header className="App-header">
         <MenuBar />
-        {/* <displayHome /> */}
       </header>
     </div>
   );
