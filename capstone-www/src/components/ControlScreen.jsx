da0d2c64db38cc3126493cefc5562cae54cf51a4
Alyssa Frost <frost7@uab.edu>
2022-04-20 20:18:56 -0500

    Add more sensors

--- capstone-www/src/components/ControlScreen.jsx
+++ capstone-www/src/components/ControlScreen.jsx
@@ -25,7 +25,7 @@ const ControlScreen = () => {
     <Layout>
       {" "}
       {"Switch Toggles"}
-      <Card title="Kitchen Appliances" align="horizontal">
+      <Card title="Kitchen Appliances & Sensors" align="horizontal">
         <Space>
           <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
@@ -39,9 +39,13 @@ const ControlScreen = () => {
           <ApplianceToggle></ApplianceToggle>
           <h> Microwave </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Window 1 </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Window 2 </h>
+          <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
-      <Card title="Living Room Appliances" align="horizontal">
+      <Card title="Living Room Appliances & Sensors" align="horizontal">
         <Space>
           <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
@@ -51,9 +55,15 @@ const ControlScreen = () => {
           <ApplianceToggle></ApplianceToggle>
           <h> Television </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Window 1 </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Window 2 </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Window 3 </h>
+          <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
-      <Card title="Main Bedroom Appliances" align="horizontal">
+      <Card title="Main Bedroom Appliances & Sensors" align="horizontal">
         <Space>
           <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
@@ -63,9 +73,15 @@ const ControlScreen = () => {
           <ApplianceToggle></ApplianceToggle>
           <h> Television </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Window 1 </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Window 2 </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Window 3 </h>
+          <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
-      <Card title="Kids Bedroom Appliances" align="horizontal">
+      <Card title="Kids Bedroom Appliances & Sensors" align="horizontal">
         <Space>
           <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
@@ -73,29 +89,57 @@ const ControlScreen = () => {
           <ApplianceToggle></ApplianceToggle>
           <h> Lamp 2 </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Window 1 </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Window 2 </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Window 3 </h>
+          <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
-      <Card title="Main Bathroom" align="horizontal">
+      <Card title="Main Bathroom Appliances & Sensors" align="horizontal">
         <Space>
           <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
           <h> Exhaust Fan </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Window </h>
+          <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
-      <Card title="Kids Bathroom" align="horizontal">
+      <Card title="Kids Bathroom Appliances & Sensors" align="horizontal">
         <Space>
           <h> Overhead light </h>
           <ApplianceToggle></ApplianceToggle>
           <h> Exhaust Fan </h>
           <ApplianceToggle></ApplianceToggle>
+          <h> Window </h>
+          <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
-      <Card title="Wash Room" align="horizontal">
+      <Card title="Wash Room Appliances & Sensors" align="horizontal">
         <Space>
+          <h> Overhead light </h>
+          <ApplianceToggle></ApplianceToggle>
           <h> Washing Machine </h>
           <ApplianceToggle></ApplianceToggle>
           <h> Dryer </h>
+          <ApplianceToggle></ApplianceToggle>
+        </Space>
+      </Card>
+      <Card title="External Doors" align="horizontal">
+        <Space>
+          <h> Front Door </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Back Door </h>
+          <ApplianceToggle></ApplianceToggle>
+          <h> Garage Door </h>
+          <ApplianceToggle></ApplianceToggle>
+        </Space>
+      </Card>
+      <Card title="HVAC on/off" align="horizontal">
+        <Space>
+          <h> HVAC </h>
           <ApplianceToggle></ApplianceToggle>
         </Space>
       </Card>
